879b8f72a34546ccda0b475bb7d86cc67d4dbd42
Girish Singh <girish.singh@caterwow.com>
2015-12-09 18:30:10 +0530

    Committing Vendor
    Committing the entire vendor directory to the source.

---
+++ vendor/laravel/lumen-framework/config/db_table_names.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+Storing all the globals in one place for making it easier to update.
+The file will contain all environment level variables.
+*/
+
+return [
+'state' => 'state',
+'city' => 'city',
+'locality' => 'locality',
+'locality_serve' => 'locality_serve',
+'company' => 'company',
+'company_location' => 'company_location',
+'customer'=>'customer',
+'restaurant_company'=>'restaurant_company',
+'restaurant'=>'restaurant',
+'restaurant_view' => 'restaurant_view',
+'restaurant_images' => 'restaurant_images',
+'restaurant_schedule' => 'restaurant_schedule',
+'menu'=>'menu',
+'menu_item'=>'menu_item',
+'menu_item_option' => 'menu_item_option',
+'menu_item_option_list' => 'menu_item_option_list',
+'filter'=>'filter',
+'filter_list'=>'filter_list',
+'order'=>'order',
+'order_menu'=>'order_menu',
+'order_menu_list'=>'order_menu_list',
+'order_menu_item_option'=>'order_menu_item_option',
+'order_menu_item_option_list' => 'order_menu_item_option_list',
+];
---
+++ vendor/laravel/lumen-framework/config/globals.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Storing all the globals in one place for making it easier to update.
+The file will contain all environment level variables.
+*/
+
+return [
+'api_path' => '/api/v001',
+'error_msg' => 'Internal Server Error. Please contact CaterWow customer support to report the error',
+'time_zone' => 'Asia/Calcutta',
+];
